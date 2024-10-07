0294a65b9ac4dffce21dca03d01fb6b6bd09ffd8
mohammadhhhhhh <9025722@student.zadkine.nl>
2024-10-07 15:57:52 +0200

    Add files via upload

---
+++ week2/app.js
@@ -0,0 +1,58 @@
+let heading = document.getElementById('heading')
+
+function changetext() {
+heading.innerText = "goedeavond";
+heading.style.color = "red" ;
+heading.style.backgroundColor = "yellow"
+
+
+
+
+
+
+    
+}
+
+
+
+function addnumber(num1, num2) {
+console.log(num1 * num2);
+
+
+
+
+
+}
+
+
+addnumber(2,8)
+
+
+
+
+
+function multiplyByFive(num) {
+
+
+console.log(num * 5);
+
+
+
+
+}
+
+
+multiplyByFive(7)
+
+
+
+function calculateminutes(num) {
+
+    console.log(num / 60);
+
+
+
+
+
+}
+calculateminutes(7200)
